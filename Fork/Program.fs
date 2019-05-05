791fb0ee063888dac24ed0bcb02b0a9cbb4194ab
Robert Andersson <robert.andersson.swe@gmail.com>
2019-05-05 19:12:42 +0200

    Handle parse error case

--- Fork/Program.fs
+++ Fork/Program.fs
@@ -36,7 +36,9 @@ let main argv =
                   |> Result.Ok
                   |> Result.bind (fun x -> if File.Exists x then Result.Ok x else Result.Error(sprintf "Could not find file '%s'." x))
                   |> Result.map File.ReadAllText
-                  |> Result.map ProcessStartInfoProvider.Parse // This could throw, should be try catched in a Result.Bind
+                  |> Result.bind (fun x ->
+                     try Result.Ok(ProcessStartInfoProvider.Parse x)
+                     with e -> Result.Error e.Message)
                   |> Result.map (Seq.map (fun x -> {
                       Alias = x.Alias
                       Tasks = x.Tasks |> Array.map (fun x -> {
@@ -57,14 +59,14 @@ let main argv =
     | Ok tasks ->
                     let processWithStdout x = ProcessHandler.Create x (fun y -> ColoredPrintf.colorprintfn "%s $yellow[->] %s" x.Alias y.Data) Console.WriteLine
                     let processes = tasks |> List.map (fun x -> { Processes = x.Tasks |> List.map processWithStdout; Alias = x.Alias })
-                    
+
                     let aliases = processes
                                   |> List.collect (fun x -> x.Processes)
                                   |> List.map (fun x -> x.Alias)
                                   |> List.append (processes |> List.map (fun x -> x.Alias))
-                                  
+
                     let inputAnalyzer x = InputAnalyzer.ParseCommand x aliases
-                    
+
                     {
                         InputFunction = Console.ReadLine >> inputAnalyzer
                         OutputFunction = Console.WriteLine
