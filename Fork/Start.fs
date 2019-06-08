849e17ea2caf289271f5a0f3a69d2096a9efaaf8
Robert Andersson <robert.andersson.swe@gmail.com>
2019-06-08 13:07:08 +0200

    Do not start aliases that have allready been started

--- Fork/Start.fs
+++ Fork/Start.fs
@@ -1,5 +1,6 @@
 module internal Command.Start
 open ProcessHandler
+open ProcessHandler
 open State
 
 let internal search (processes : StartInfo list) (input : string) =
@@ -15,11 +16,12 @@ let internal search (processes : StartInfo list) (input : string) =
         then (noAliasGroupSearch(), input) |> SearchResult.Alias
         else (search |> List.collect (fun x -> x.Processes), input) |> SearchResult.AliasGroup
 
-let internal Exec input processes activeProcesses startProcess =
+let internal Exec input processes (activeProcesses : FProcess list) startProcess =
     match search processes input with
                     | Alias(x, y) -> match x with
                                      | Some x -> [ x ]
                                      | None -> []
                     | AliasGroup(x, y) -> x
+                    |> List.filter (fun x -> not (activeProcesses |> List.exists (fun y -> y.Alias <> x.Alias)))
                     |> List.map (fun x -> x.Arguments |> startProcess)
                     |> (fun x -> activeProcesses @ x)
